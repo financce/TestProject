1bf8bedf36bb91e75c8e88eb1d514d2208ac3cdd
Vyacheslav <financce@gmail.com>
2021-06-05 13:18:21 +0300

    updated variants

--- TestProject/View/DetailView.swift
+++ TestProject/View/DetailView.swift
@@ -12,6 +12,7 @@ struct DetailView: View {
     var name: String
     @EnvironmentObject var obs: Observer
     @State private var model = ModelAppList(name: "", nameView: "", image: "", selector: 0, variants: [Variant]())
+    @State private var chooise = 0
     
     var body: some View {
         Form{
@@ -23,10 +24,16 @@ struct DetailView: View {
                 AnimatedImage(url: URL(string: model.image))
                     .resizable()
                     .frame(width: 50, height: 50)
+                Picker("Choise", selection: $chooise) {
+                    ForEach(0 ..< model.variants.count) {
+                        Text("\(self.model.variants[$0].text)")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
             }
         }
         .onAppear{
             model = obs.data.filter{$0.nameView == name}.first ?? .init(name: "", nameView: "", image: "", selector: 0, variants: [Variant]())
+            self.chooise = obs.data.filter{$0.nameView == name}.first?.selector ?? 0
         }
     }
 }
